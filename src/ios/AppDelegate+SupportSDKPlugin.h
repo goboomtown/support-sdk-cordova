168da90df8cd1d482f23793d7756353e9dfa910d
Larry Borsato <larry@larryborsato.com>
2020-12-18 15:17:03 -0500

    Added some  issing files.

---
+++ src/ios/AppDelegate+SupportSDKPlugin.h
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.h
+//
+//
+//  Created by Larry Borsato on 2020-09-20.
+//
+
+#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+#import <Cordova/CDV.h>
+#import "ViewController.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppDelegate (supportSDKPlugin)
+
+
+// @property (strong, nonatomic, nullable) ViewController *btViewController;
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey, id> *)launchOptions;
+
+@end
+
+NS_ASSUME_NONNULL_END
